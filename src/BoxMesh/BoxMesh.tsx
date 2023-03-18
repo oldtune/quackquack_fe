b68aabd546babf39f31d9073761c7f46d72c1633
QuackQuack <quackablequackquack@gmail.com>
2023-03-18 22:57:41 +0700

    add box mesh rotation

--- src/BoxMesh/BoxMesh.tsx
+++ src/BoxMesh/BoxMesh.tsx
@@ -1,12 +1,31 @@
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
 
 export const BoxMesh: any = () => {
-  let three = useFrame(() => {
-    console.log("spinning");
+  let { camera } = useThree();
+  let boxRef: any = useRef();
+  let [goNear, setgoNear] = useState(true);
+  useFrame(({ clock }) => {
+    let elapsedTime = clock.getElapsedTime();
+    let elapsedTimeTenThousand = elapsedTime / 10000;
+
+    boxRef.current.rotation.x = elapsedTime;
+    boxRef.current.rotation.y = elapsedTime;
+    boxRef.current.rotation.z = elapsedTime;
+
+    if (boxRef.current.position.z == 0 || boxRef.current.position.z == 5) {
+      setgoNear(!goNear);
+    }
+
+    if (goNear) {
+      boxRef.current.position.z += elapsedTimeTenThousand;
+    } else {
+      boxRef.current.position.z -= elapsedTimeTenThousand;
+    }
   });
 
   return (
-    <mesh>
+    <mesh ref={boxRef}>
       <boxGeometry />
       <meshStandardMaterial />
     </mesh>
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -5,7 +5,9 @@ export const Navbar: React.FC = () => {
   return (
     <Header>
       <div id="logo" />
-      <Menu theme="dark"></Menu>
+      <Menu theme="dark">
+        <Menu.Item key={1}>Home</Menu.Item>
+      </Menu>
     </Header>
   );
 };
